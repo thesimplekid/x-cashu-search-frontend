6046ad05b40695ed6c4da0f2b5104b530f512ab6
thesimplekid <tsk@thesimplekid.com>
2024-05-29 10:39:49 +0100

    feat: loading spinner

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -122,7 +122,7 @@
   }
 
   async function handleKeyup(e) {
-    if (e.keyCode == 13) {
+    if (e.keyCode == 13 && search_count != undefined && search_count > 0) {
       await handleSearch();
     }
   }
@@ -133,73 +133,82 @@
 </script>
 
 <div class="min-h-screen bg-gray-800 text-gray-100">
-  <header class="p-4 dark:bg-gray-800 dark:text-gray-900">
-    <div class="container flex justify-start h-16 mx-auto">
-      <button
-        type="button"
-        class="bg-purple-800 hover:bg-purple-600 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg px-8 py-5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55"
-        on:click={home}
-      >
-        <svg
-          data-v-52a72b4a=""
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          class="w-10 h-10 fill-gray-300"
+  {#if search_results.length === 0}
+    <div class="container flex justify-center h-16 mx-auto bg-gray-800">
+      <div
+        class="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-600"
+      ></div>
+    </div>
+  {:else}
+    <header class="p-4 dark:bg-gray-800 dark:text-gray-900">
+      <div class="container flex justify-start h-16 mx-auto">
+        <button
+          type="button"
+          class="bg-purple-800 hover:bg-purple-600 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg px-8 py-5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55"
+          on:click={home}
         >
-          <path
-            d="M12.827 3.834a1.5 1.5 0 00-2.26.054L5 10.58V19.5A1.5 1.5 0 006.5 21h1.79a1.5 1.5 0 001.5-1.5v-3.011a1.5 1.5 0 011.5-1.5h1.42a1.5 1.5 0 011.5 1.5V19.5a1.5 1.5 0 001.5 1.5h1.79a1.5 1.5 0 001.5-1.5v-8.92l-6.173-6.746z"
-          ></path></svg
-        >
-      </button>
-      <div class="container flex justify-center h-16 mx-auto">
-        {#if search_count != undefined && search_count > 0}
-          <label for="Search" class="hidden">Search</label>
-          <input
-            type="text"
-            autocomplete="off"
-            placeholder="Search..."
-            class="w-2/3 rounded-lg focus:outline-none bg-gray-600 text-gray-800 focus:bg-gray-500 focus:border-violet-600"
-            bind:value={search_query}
-            on:keyup={handleKeyup}
-          />
-
-          <a
-            href="/search?q={search_query}"
-            on:click={handleSearch}
-            class="px-8 py-5 font-semibold rounded-lg bg-purple-800 hover:bg-purple-600 text-gray-100 text-center"
-            >Go
-          </a>
-        {/if}
-      </div>
-      <div class="container flex justify-end h-16 mx-auto">
-        <div class="items-center">
-          {#if $cost_per_search != undefined && balance != undefined}
-            <button
-              class="px-8 py-5 font-semibold rounded dark:bg-gray-800 dark:text-gray-100"
-              >{search_count}</button
-            >
-          {/if}
-          <a
-            href="/topup"
-            class="px-8 py-5 text-center font-semibold rounded dark:bg-purple-800 dark:text-gray-100 hover:bg-purple-500 disabled:bg-gray-500"
-            >Top Up</a
+          <svg
+            data-v-52a72b4a=""
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="w-10 h-10 fill-gray-300"
           >
+            <path
+              d="M12.827 3.834a1.5 1.5 0 00-2.26.054L5 10.58V19.5A1.5 1.5 0 006.5 21h1.79a1.5 1.5 0 001.5-1.5v-3.011a1.5 1.5 0 011.5-1.5h1.42a1.5 1.5 0 011.5 1.5V19.5a1.5 1.5 0 001.5 1.5h1.79a1.5 1.5 0 001.5-1.5v-8.92l-6.173-6.746z"
+            ></path></svg
+          >
+        </button>
+        <div class="container flex justify-center h-16 mx-auto">
+          {#if search_count != undefined && search_count > 0}
+            <label for="Search" class="hidden">Search</label>
+            <input
+              type="text"
+              autocomplete="off"
+              placeholder="Search..."
+              class="w-2/3 rounded-lg focus:outline-none bg-gray-600 text-gray-800 focus:bg-gray-500 focus:border-violet-600"
+              bind:value={search_query}
+              on:keyup={handleKeyup}
+            />
+
+            <a
+              href="/search?q={search_query}"
+              on:click={handleSearch}
+              class="px-8 py-5 font-semibold rounded-lg bg-purple-800 hover:bg-purple-600 text-gray-100 text-center"
+              >Go
+            </a>
+          {/if}
+        </div>
+        <div class="container flex justify-end h-16 mx-auto">
+          <div class="items-center">
+            {#if $cost_per_search != undefined && balance != undefined}
+              <button
+                class="px-8 py-5 font-semibold rounded dark:bg-gray-800 dark:text-gray-100"
+                >{search_count}</button
+              >
+            {/if}
+            <a
+              href="/topup"
+              class="px-8 py-5 text-center font-semibold rounded dark:bg-purple-800 dark:text-gray-100 hover:bg-purple-500 disabled:bg-gray-500"
+              >Top Up</a
+            >
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-  <div class="container flex flex-col mx-auto">
-    {#each search_results as search_result}
-      <div class="container flex flex-col my-3">
-        <h3
-          class="text-xl text-bold text-purple-500 hover:text-purple-400 hover:underline"
-        >
-          <a href={search_result.url} class="text-bold">{search_result.title}</a
+    </header>
+    <div class="container flex flex-col mx-auto">
+      {#each search_results as search_result}
+        <div class="container flex flex-col my-3">
+          <h3
+            class="text-xl text-bold text-purple-500 hover:text-purple-400 hover:underline"
           >
-        </h3>
-        <p class="text-sm font-light text-white">{search_result.url}</p>
-        <p class="text-lg">{search_result.description}</p>
-      </div>
-    {/each}
-  </div>
+            <a href={search_result.url} class="text-bold"
+              >{search_result.title}</a
+            >
+          </h3>
+          <p class="text-sm font-light text-white">{search_result.url}</p>
+          <p class="text-lg">{search_result.description}</p>
+        </div>
+      {/each}
+    </div>
+  {/if}
 </div>
